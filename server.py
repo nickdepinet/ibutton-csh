db69aeee96f476c02decc1e5c11e369bc58ab8d2
Nick Depinet <depinetnick@gmail.com>
2013-09-18 13:21:59 -0400

    Start of ldap stuff

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
 import tornado.web
+import ldap
 
 class IbuttonHandler(tornado.web.RequestHandler):
 
@@ -7,7 +8,16 @@ class IbuttonHandler(tornado.web.RequestHandler):
         # Do the actual work here
         # Call out to ldap, return a json dict
         # contents: entryUUID, username
+        response = {
+            'username': '',
+            'entryUUID': ''
+            }
 
+        self.write(response)
+
+
+def ldap_search(ibutton):
+    # Do the ldap search here    
 
 if __name__ == "__main__":
     application = tornado.web.Application([
